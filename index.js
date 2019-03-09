92e8b5052a1781009bbfa787e51accd4262d5bde
Andrew Chow <andrew.chow@blackrock.com>
2019-03-09 23:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toLowerCase()
-    return "I can't hear you!"
+  if string.toLowerCase() {
+    return "I can't hear you!";
   string.toUpperCase()
     return "YES INDEED!"
   
